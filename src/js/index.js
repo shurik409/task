d4e3db441ed40b477878acb2687c4ae053580ef3
shurik409 <shurik409@gmail.com>
2018-10-31 00:48:51 +0300

    home fetch

--- src/js/index.js
+++ src/js/index.js
@@ -1,80 +1,76 @@
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-readTextFile("../persons.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data);
-});
 
-let psLeng = Object.keys(persons).length;
-console.log(psLeng);
-let onPage = 5;
-let cnt_page = Math.ceil(psLeng / onPage); 
+let persons =" ";
+fetch('src/persons.json')
+.then( resp => resp.json() )
+.then( json => {
+    console.log(JSON.stringify(json) + "SSS");
+    pagin(json);
+}); 
 
-console.log(cnt_page);
+function pagin(persons){
+
+    let psLeng = Object.keys(persons).length;
+    console.log(psLeng);
+    let onPage = 5;
+    let cnt_page = Math.ceil(psLeng / onPage); 
+
+    console.log(cnt_page);
 
 
-
-for(p in persons){
-    if(p<onPage){
-        let div = document.createElement("div");
-
-        div.className = "person";
-        div.innerHTML = persons[p].name + " " + persons[p].surname;
-        console.log(JSON.stringify(persons[p].name));
-        document.getElementById("pers").appendChild(div);
-    }
-}
-
-let page = "" 
-for(let i = 0; i < cnt_page; i++){
-    page += "<span data-page=" + i * onPage + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
-
-}
-document.getElementById("pagination").innerHTML = page;
-
-
-let main_page = document.getElementById("page1");
-main_page.classList.add("paginator_active");
-
-window.page = function(event) {
-    let e = event || window.event;
-    let target = e.target;
-    let id = target.id;
-    console.log(target + " " + id);
-
-
-    if (target.tagName.toLowerCase() != "span") return;
-
-    let num = (target.innerHTML - 1) * onPage;
-    console.log(num);
-
-    old = document.getElementsByClassName("person");
-    console.log(old.length);
-    while(old[0])
-        old[0].parentNode.removeChild(old[0]);
 
     for(p in persons){
-        if(p>num-1 && p<num+onPage){
+        if(p<onPage){
             let div = document.createElement("div");
-    
+
             div.className = "person";
             div.innerHTML = persons[p].name + " " + persons[p].surname;
             console.log(JSON.stringify(persons[p].name));
             document.getElementById("pers").appendChild(div);
         }
     }
-    main_page.classList.remove("pagination_active");
-    main_page = document.getElementById(id);
-    main_page.classList.add("pagination_active");
-    
-}
 
+    let page = "" 
+    for(let i = 0; i < cnt_page; i++){
+        page += "<span data-page=" + i * onPage + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
+
+    }
+    document.getElementById("pagination").innerHTML = page;
+
+
+    let main_page = document.getElementById("page1");
+    main_page.classList.add("paginator_active");
+
+    window.page = function(event) {
+        let e = event || window.event;
+        let target = e.target;
+        let id = target.id;
+        console.log(target + " " + id);
+
+
+        if (target.tagName.toLowerCase() != "span") return;
+
+        let num = (target.innerHTML - 1) * onPage;
+        console.log(num);
+
+        old = document.getElementsByClassName("person");
+        console.log(old.length);
+        while(old[0])
+            old[0].parentNode.removeChild(old[0]);
+
+        for(p in persons){
+            if(p>num-1 && p<num+onPage){
+                let div = document.createElement("div");
+        
+                div.className = "person";
+                div.innerHTML = persons[p].name + " " + persons[p].surname;
+                console.log(JSON.stringify(persons[p].name));
+                document.getElementById("pers").appendChild(div);
+            }
+        }
+        main_page.classList.remove("pagination_active");
+        main_page = document.getElementById(id);
+        main_page.classList.add("pagination_active");
+        
+    }
+
+}
